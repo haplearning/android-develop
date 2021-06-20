6fa14c8714791851f8b5c926be1a1830e8a0e3b9
donguaa <donguaa@donguaa.com>
2021-06-20 23:00:45 +0800

    rename the packaged name

--- smalilog/app/src/main/java/com/example/smalilog/MainActivity.java
+++ smalilog/app/src/main/java/com/example/smalilog/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         name = "丑小鸭";
         switch (name){
             case "丑小鸭":
@@ -27,6 +28,25 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("hello","没有符合的 name 值");
                 break;
         }
+        fun1();
+        fun2();
+        fun3();
+        Log.i("这个值是",String.valueOf(fun1()));
+    }
 
+    public int fun1(){
+        int value = Test.value; //888
+        return value;
+    }
+    public int fun2(){
+        int value2 = Test.value2; //777
+        return value2;
+    }
+
+    public String fun3(){
+        String str = Test.str; //www.52pojie.cn
+        String str2 = Test.str2; //码完代码去看东方明珠
+        int value3 = Test.value3; //666
+        return str2;
     }
 }
---
+++ smalilog/app/src/main/java/com/example/smalilog/Test.java
@@ -0,0 +1,9 @@
+package com.example.smalilog;
+
+public class Test {
+    public static String str="www.52pojie.cn";
+    public static String str2="码完代码去看东方明珠";
+    public static int value = 888;
+    public static int value2 = 777;
+    public static int value3 = 666;
+}
